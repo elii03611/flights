e092f56f1977a92d1f9dc9211e481f5f073139e3
elii03611 <elii03611@gmail.com>
2023-05-19 15:16:49 +0300

    add about page

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -9,7 +8,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Flights.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exc:      
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
