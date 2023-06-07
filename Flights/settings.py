a4ab3ad2b8b3457418c6d8b5fe191bf8dbc2e10e
elii03611 <elii03611@gmail.com>
2023-06-07 21:42:26 +0300

    add about page

--- Flights/settings.py
+++ Flights/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/company_airline/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
