f4e33df2ee38234701e1940ebe7f7c163fdc93d8
elii03611 <elii03611@gmail.com>
2023-06-07 22:16:33 +0300

    add about page

--- Flights/settings.py
+++ Flights/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4_ajtd*1g_9r(8#5!*az3pxvxuurs^g0qqv4%ri!=ud^cg&uja'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,8 +127,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
 
 
 
