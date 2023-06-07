8d3d2f69f26a29c8023754910bd9e41fde5111b2
elii03611 <elii03611@gmail.com>
2023-06-07 22:02:03 +0300

    add about page

--- Flights/settings.py
+++ Flights/settings.py
@@ -129,8 +129,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
 
 
 STATIC_URL = '/static/'
@@ -139,13 +138,6 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
