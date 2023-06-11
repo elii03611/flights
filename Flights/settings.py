1f1324c3defd8454f942ffb0210938c81836ebc9
elii03611 <elii03611@gmail.com>
2023-06-11 15:19:15 +0300

    add about page

--- Flights/settings.py
+++ Flights/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = 'static/'
+STATIC_URL = 'css/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'css/'),)
 
 
 
