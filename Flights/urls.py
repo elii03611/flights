96285a437dda028b93f54bb520309c4b345fc84f
elii03611 <elii03611@gmail.com>
2023-06-14 13:00:49 +0300

    add about page

--- Flights/urls.py
+++ Flights/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('company_airline.urls')),
@@ -24,4 +25,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('company_airline/', include('company_airline.urls')),
     path('staff/', include('staff.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
