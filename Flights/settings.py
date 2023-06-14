bc8047c2c6893bcf85f772abecd916d4916067cc
elii03611 <elii03611@gmail.com>
2023-06-14 12:34:43 +0300

    add about page

--- Flights/settings.py
+++ Flights/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static"
+STATIC_ROOT = "/https://flights-6ham.onrender.com/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "company_airline/static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
